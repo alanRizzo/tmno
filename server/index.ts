db2df8a6acd3855b34455e005a931ffc6af9bd65
Alan <alan.rizzo@gmail.com>
2025-02-07 01:56:53 +0000

    try env issue

--- server/index.ts
+++ server/index.ts
@@ -53,7 +53,9 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    serveStatic(app);
+    await setupVite(app, server);
+    // serveStatic(app);
+    // FIX THIS!
   }
 
   // ALWAYS serve the app on port 5000
