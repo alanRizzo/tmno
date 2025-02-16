33590f7b1b430af695059b8028bad43d09ce7de0
Alan Rizzo <alan.rizzo@gmail.com>
2025-02-16 20:57:19 -0300

    logo loco

--- client/src/components/HeaderSection.tsx
+++ client/src/components/HeaderSection.tsx
@@ -1,6 +1,7 @@
 import { LuMapPinHouse, LuInstagram } from "react-icons/lu";
 import IconLink from "./ui/IconLink";
 import { useDevice } from "@/hooks/DeviceContext.tsx";
+import { motion } from "framer-motion";
 
 export default function HeaderSection() {
 	const { isMobile } = useDevice();
@@ -8,15 +9,30 @@ export default function HeaderSection() {
 	return (
 		<div className="relative h-screen">
 			<div className="flex justify-center items-center h-full">
-				<img
-					src="/logo.svg"
-					alt="TMNO"
-					className="w-80 h-80 md:w-96 md:h-96 invert"
-				/>
+				<motion.div
+					className="relative"
+					initial={{ opacity: 0, scale: 0.8 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 1, ease: "easeOut" }}
+					whileHover={{ scale: 1.05 }}
+					whileTap={{ scale: 0.95 }}
+				>
+					<img
+						src="/logo.svg"
+						alt="TMNO"
+						className="w-80 h-80 md:w-96 md:h-96 invert glitch"
+					/>
+					{/* Clones para el glitch */}
+					<img src="/logo.svg" alt="glitch" className="glitch-img glitch-red" />
+					<img
+						src="/logo.svg"
+						alt="glitch"
+						className="glitch-img glitch-blue"
+					/>
+				</motion.div>
 			</div>
 
 			{isMobile ? (
-				// Mobile
 				<>
 					<div className="absolute top-10 left-10">
 						<IconLink
@@ -34,7 +50,6 @@ export default function HeaderSection() {
 					</div>
 				</>
 			) : (
-				// Desktop
 				<div className="absolute right-10 top-1/2 transform -translate-y-1/2 flex flex-col gap-4">
 					<IconLink
 						href="https://www.instagram.com/tumadrenosodia/"
