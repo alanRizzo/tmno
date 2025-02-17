1d35f2126fd8f3dc17d4d00b4a05d0325a2a32da
Alan Rizzo <alan.rizzo@gmail.com>
2025-02-16 23:22:59 -0300

    vercel speed insights

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
-import React from "react";
 import FullPageComponent from "@/components/ScrollPage.tsx";
 import { DeviceProvider } from "@/hooks/DeviceContext.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import React from "react";
 
 function App() {
 	return (
 		<DeviceProvider>
+			<SpeedInsights />
 			<FullPageComponent />
 		</DeviceProvider>
 	);
