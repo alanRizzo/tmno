66002093e2b1eeb23248d9dbdcb2e2b5f05db4fa
Alan Rizzo <alan.rizzo@gmail.com>
2025-02-17 14:27:07 -0300

    remove something

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { injectSpeedInsights } from "@vercel/speed-insights";
-
-injectSpeedInsights();
 
 createRoot(document.getElementById("root") as HTMLElement).render(<App />);
